14c9f5321f11362af3f9cc882342626f17f63014
Scelio Life Sciences <sceliolifesciences@gmail.com>
2025-04-18 21:28:08 +0530

    new code added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sceliolifesciences/", // Updated to match your repository name
+  base: "/", // Set for root domain deployment
   plugins: [react()],
 })
